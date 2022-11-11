a102c484125561316e5729f4f6655546758d272f
N1V33R4 <75680359+N1V33R4@users.noreply.github.com>
2022-11-11 17:26:55 +0700

    Add classroom tables

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -21,10 +21,12 @@ class Classroom extends Model
     protected $guarded = ['id'];
     protected $fillable = [
         'name',
+        'department_id',
         'room_id',
         'course_program_id',
         'start_date',
         'end_date',
+        'batch',
         'year',
         'semester',
         'status'
@@ -37,7 +39,18 @@ class Classroom extends Model
     | FUNCTIONS
     |--------------------------------------------------------------------------
     */
-
+    /**
+     * Clone the model into a new, non-existing instance.
+     *
+     * @param  array|null  $except
+     * @return static
+     */
+    public function replicate(array $except = null)
+    {
+        $clone = parent::replicate();
+        $clone->name .= ' (copy)';
+        return $clone;
+    }
     /*
     |--------------------------------------------------------------------------
     | RELATIONS
@@ -74,7 +87,58 @@ class Classroom extends Model
     | ACCESSORS
     |--------------------------------------------------------------------------
     */
+    public function getLecturersListAttribute() {
+        return json_encode($this->lecturers->map(function($lecturer) {
+            return ['lecturer_id' => $lecturer->id, 'order' => $lecturer->pivot->order];
+        }));
+    }
 
+    public function getLecturersCoursesTableAttribute() {
+        $courses = $this->courseProgram->courses->sortBy('pivot.order')->pluck('name');
+        $lecturers = $this->lecturers->sortBy('pivot.order')->pluck('name');
+
+        $table = [
+            ['Mon' => $courses[0], 'Tue' => $courses[1], 'Wed' => $courses[2], 'Thu' => $courses[3], 'Fri' => $courses[4]],
+            ['Mon' => $lecturers[0], 'Tue' => $lecturers[1], 'Wed' => $lecturers[2], 'Thu' => $lecturers[3], 'Fri' => $lecturers[4]]
+        ];
+        return json_encode($table);
+    }
+
+    public function getStatusAttribute($value) {
+        return ucfirst($value);
+    }
+
+    // TABLES
+    public function getMorningStudentsTableAttribute() {
+        $morning_id = Shift::where('name', 'Morning')->first()->id;
+        $studentGroup = $this->studentGroups->where('shift_id', $morning_id)->first();
+        if ($studentGroup == null) 
+            return;
+
+        return json_encode($studentGroup->students->sortBy('name')->values()->map(function($student, $key) {
+            return ['count' => $key + 1, 'name' => $student->name, 'birthdate' => $student->birthdate];
+        }));
+    }
+    public function getAfternoonStudentsTableAttribute() {
+        $afternoon_id = Shift::where('name', 'Afternoon')->first()->id;
+        $studentGroup = $this->studentGroups->where('shift_id', $afternoon_id)->first();
+        if ($studentGroup == null) 
+            return;
+
+        return json_encode($studentGroup->students->sortBy('name')->values()->map(function($student, $key) {
+            return ['count' => $key + 1, 'name' => $student->name, 'birthdate' => $student->birthdate];
+        }));
+    }
+    public function getEveningStudentsTableAttribute() {
+        $evening_id = Shift::where('name', 'Evening')->first()->id;
+        $studentGroup = $this->studentGroups->where('shift_id', $evening_id)->first();
+        if ($studentGroup == null) 
+            return;
+
+        return json_encode($studentGroup->students->sortBy('name')->values()->map(function($student, $key) {
+            return ['count' => $key + 1, 'name' => $student->name, 'birthdate' => $student->birthdate];
+        }));
+    }
     /*
     |--------------------------------------------------------------------------
     | MUTATORS
